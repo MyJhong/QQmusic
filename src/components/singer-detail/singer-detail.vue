d110262471c2849ea4e5e4d5d0bd4b7679b26684
Jhong <804381249@qq.com>
2019-06-03 12:03:44 +0800

    music -list

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,13 +1,57 @@
 <template>
-  <div class="slide">1</div>
+  <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
 </template>
 
 <script type="text/ecmascript-6">
 import { mapGetters } from 'vuex'
+import { ERR_OK } from 'api/config'
+import {getSingerDetail} from 'api/singer'
+import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
     ...mapGetters(['singer'])
+  },
+  created() {
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.data.list)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
@@ -15,13 +59,4 @@ export default {
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import '~common/stylus/variable';
 
-.slide {
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: $color-background;
-}
 </style>
